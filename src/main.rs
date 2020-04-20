4694e80c9271ab07dce915a7a5ad111babcf0524
Ari Krumbein <ari.krumbein@gmail.com>
2020-04-19 23:52:26 -0700

    fix OOB error with arguments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
 
@@ -16,9 +16,14 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
