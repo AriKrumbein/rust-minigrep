fa661cc2772d4950ddf3855ea676ce2f42f8c48c
Ari Krumbein <ari.krumbein@gmail.com>
2020-04-20 00:04:32 -0700

    use error codes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
 
@@ -17,13 +21,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // &'static is the type of string literals, static program duration
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments!");
+            return Err("Not enough arguments!");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
