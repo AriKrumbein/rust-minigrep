0b844b7ad5e3375716800b65ca78a4fd94d61e29
Ari Krumbein <arkrumbe@microsoft.com>
2020-05-09 21:38:17 -0700

    moved logic to un function

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,13 @@ use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args)
+    .unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
-
-    println!("Contents:\n{}", contents);
+    run(config);
 }
 
 #[allow(dead_code)]
@@ -28,7 +26,14 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-
+        
         Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
+    
+    println!("Contents:\n{}", contents);
 }
